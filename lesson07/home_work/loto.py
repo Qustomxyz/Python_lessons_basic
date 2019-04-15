d0f754fd8bb5fe011ffeebace7500f716ec5003a
Vladimir <46341690+Qustomxyz@users.noreply.github.com>
2019-04-16 01:05:59 +0300

    Update loto.py
    Сделал лото

--- lesson07/home_work/loto.py
+++ lesson07/home_work/loto.py
@@ -57,3 +57,94 @@
 модуль random: http://docs.python.org/3/library/random.html
 
 """
+import random
+import re
+
+
+class Lotto:
+    def __init__(self):
+        self.step = 90
+        self.card_1 = self.make_player_card(player='player')
+        self.card_2 = self.make_player_card()
+        empty_pattern = re.compile(r'^\D+$')
+        human_winner_message = 'Стоп игра!\nПобеду одержал Человек!'
+        computer_winner_message = 'Стоп игра!\nПобеду одержал Компьютер!'
+        tie_message = 'Стоп игра!\nУникальный случай!!!\n!!!НИЧЬЯ!!!\n'
+
+        for keg in self.take_keg():
+            self.step -= 1
+            print(f'Новый бочонок: {keg} (осталось {self.step})')
+            print(self.card_1)
+            print(self.card_2)
+            player_answer = input('Зачеркнуть цифру? (y/n): ')
+            if player_answer in ('y', 'yes', 'да', 'д'):
+                if self.check_answer('yes', keg):
+                    if empty_pattern.search(self.card_1) and empty_pattern.search(self.card_2):
+                        print(tie_message)
+                        break
+                    elif empty_pattern.search(self.card_1):
+                        print(human_winner_message)
+                        break
+                    elif empty_pattern.search(self.card_2):
+                        print(computer_winner_message)
+                        break
+                else:
+                    print(computer_winner_message)
+                    break
+            else:
+                if not self.check_answer('no', keg):
+                    print(computer_winner_message)
+                    break
+        else:
+            print('Game over!')
+
+    def check_answer(self, answer, keg):
+        for_search = re.compile(r'\b' + f'{keg}' + r'\b')
+        repl_pattern = ' -' if len(str(keg)) == 2 else '-'
+        if answer == 'yes':
+            if for_search.search(self.card_1):
+                self.card_1 = for_search.sub(repl_pattern, self.card_1)
+                self.card_2 = for_search.sub(repl_pattern, self.card_2)
+                return True
+            else:
+                return False
+        else:
+            if for_search.search(self.card_1):
+                return False
+            else:
+                self.card_2 = for_search.sub(repl_pattern, self.card_2)
+                return True
+
+    @staticmethod
+    def take_keg():
+        lotto_kegs = list(range(1, 91))
+        random.shuffle(lotto_kegs)
+        for keg in lotto_kegs:
+            yield keg
+
+    @staticmethod
+    def make_player_card(player='computer'):
+        card_numbers = '------ Ваша карточка -----\n' if player == 'player' else '-- Карточка компьютера ---\n'
+        numbers = random.sample(range(1, 91), 15)
+        slice_start = 0
+        for i in range(3):
+            slice_stop = slice_start + 5
+            numbers_for_line = numbers[slice_start:slice_stop]
+            slice_start += 5
+            spaces = random.sample(range(9), 4)
+            numbers_for_line.sort(reverse=True)
+            for j in range(9):
+                if j in spaces:
+                    card_numbers += '   '
+                else:
+                    number = str(numbers_for_line.pop())
+                    number = number if len(number) == 2 else ' ' + number
+                    card_numbers += number + ' '
+            else:
+                card_numbers += '\n'
+        card_numbers += '--------------------------\n'
+        return card_numbers
+
+
+if __name__ == '__main__':
+    game_start = Lotto()
